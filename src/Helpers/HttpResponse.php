720f922dd2ffb5dae952f76da094168992689016
Filip Šedivý <mail@filipsedivy.cz>
2018-09-01 12:08:42 +0200

    Create collection mapper

--- src/Helpers/HttpResponse.php
+++ src/Helpers/HttpResponse.php
@@ -2,16 +2,24 @@
 
 namespace FilipSedivy\DropshippingCz\Helpers;
 
-use KHerGe\JSON\JSON;
+use FilipSedivy\DropshippingCz\Exceptions\Application\UndefinedClassException;
+use FilipSedivy\DropshippingCz\Exceptions\Application\UndefinedClassParameterException;
 use Psr\Http\Message\ResponseInterface;
+use Nette\Utils\Json;
 
 class HttpResponse
 {
     private $response;
 
-    public function __construct(ResponseInterface $response)
+    private $method;
+
+    private $url;
+
+    public function __construct(ResponseInterface $response, string $method = '', string $url = '')
     {
         $this->response = $response;
+        $this->method = $method;
+        $this->url = $url;
     }
 
 
@@ -34,13 +42,84 @@ class HttpResponse
 
 
     /**
+     * @param int $options
+     *
      * @return mixed
      *
      * @throws \Nette\Utils\JsonException
      */
-    public function getJson()
+    public function toArray($options = 0)
     {
-        return \Nette\Utils\Json::decode($this->getContent());
+        return Json::decode($this->getContent(), $options);
+    }
+
+
+    /**
+     * @return mixed
+     *
+     * @throws \Nette\Utils\JsonException
+     */
+    public function toArrayData()
+    {
+        return $this->toArray(0)->data;
+    }
+
+
+    /**
+     * @return array
+     *
+     * @throws UndefinedClassParameterException
+     * @throws UndefinedClassException
+     * @throws \ReflectionException
+     * @throws \Nette\Utils\JsonException
+     */
+    public function toCollection(): array
+    {
+        if ($this->method === '')
+        {
+            throw new UndefinedClassParameterException('Parameter method is not set');
+        }
+
+        if ($this->url === '')
+        {
+            throw new UndefinedClassParameterException('Parameter url is not set');
+        }
+
+        dump($this->url);
+
+        preg_match('/^(?<method>[a-zA-Z\/\-]+)/', $this->url, $matches);
+
+        $entityClass = $this->createEntityClassName($matches['method']);
+
+        if (!class_exists($entityClass))
+        {
+            throw new UndefinedClassException($entityClass);
+        }
+
+        $collection = [];
+
+        foreach ($this->toArrayData() as $row)
+        {
+            $entityObject = new $entityClass;
+            $reflectionClass = new \ReflectionClass($entityObject);
+
+            foreach ($row as $property => $value)
+            {
+                if (!property_exists($entityClass, $property))
+                {
+                    $exceptionMessage = 'Property \'' . $property . '\' in class \'' . $entityClass . '\' not exists';
+                    throw new UndefinedClassParameterException($exceptionMessage);
+                }
+
+                $reflectionProperty = $reflectionClass->getProperty($property);
+                $reflectionProperty->setAccessible(true);
+                $reflectionProperty->setValue($entityObject, $value);
+            }
+
+            $collection[] = $entityObject;
+        }
+
+        return $collection;
     }
 
 
@@ -50,5 +129,60 @@ class HttpResponse
     public function __toString()
     {
         return $this->getContent();
+    }
+
+
+    private function createEntityClassName($method)
+    {
+        $singular = function ($input) {
+            if (false !== strpos($input, 'ies'))
+            {
+                $input = substr($input, 0, -3);
+                $input .= 'y';
+            }
+            elseif (false !== strpos($input, 'ses'))
+            {
+                $input = substr($input, 0, -3);
+                $input .= 's';
+            }
+            else
+            {
+                $input = rtrim($input, 's');
+            }
+
+            return $input;
+        };
+
+        // Detect multiname method
+        if (false !== strpos($method, '/'))
+        {
+            $explodeSeparator = explode('/', $method);
+            $method = '';
+            foreach ($explodeSeparator as $item)
+            {
+                $item = strtolower($item);
+                $item = $singular($item);
+                $method .= ucfirst($item);
+            }
+        }
+        elseif (false !== strpos($method, '-'))
+        {
+            $explodeSeparator = explode('-', $method);
+            $method = '';
+            foreach ($explodeSeparator as $item)
+            {
+                $item = strtolower($item);
+                $item = $singular($item);
+                $method .= ucfirst($item);
+            }
+        }
+        else
+        {
+            $method = $singular($method);
+            $method = ucfirst($method);
+        }
+
+
+        return 'FilipSedivy\\DropshippingCz\\Entity\\Services\\' . $method;
     }
 }
