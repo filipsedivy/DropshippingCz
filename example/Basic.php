0cc9307d7f32c56490ef871e780002de18aa45b1
Filip Šedivý <mail@filipsedivy.cz>
2018-09-01 12:07:42 +0200

    Create example

--- example/Basic.php
+++ example/Basic.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $config = new \FilipSedivy\DropshippingCz\Model\ApiConfig('....');
 $client = new \FilipSedivy\DropshippingCz\Client($config);
 
-$orders = $client->getOrders();
-$ordersList = $orders->getList();
+/** @var \FilipSedivy\DropshippingCz\Entity\Services\Order[] $collection */
+$collection = $client->getOrders()->getList()->toCollection();
